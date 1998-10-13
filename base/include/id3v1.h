b293be7f05b6e86d2b9bf2be52add2f6bc0d1ace
elrod <>
1998-10-13 11:30:59 +0000

    * broke id3v1 tag class and MediaVitalInfo out into separate headers

--- base/include/id3v1.h
+++ base/include/id3v1.h
@@ -1,4 +1,3 @@
-
 /*____________________________________________________________________________
 	
 	FreeAmp - The Free MP3 Player
@@ -22,16 +21,57 @@
 	$Id$
 ____________________________________________________________________________*/
 
-#ifndef _EVENTDATA_H_
-#define _EVENTDATA_H_
+#ifndef _ID3V1_H_
+#define _ID3V1_H_
 
 #include <iostream.h>
 #include <string.h>
 
-#include "mediavitalinfo.h"
+class Id3TagInfo {
+ public:
+    bool contains_info;
+
+    char songname[31];
+    char artist[31];
+    char album[31];
+    char year[5];
+    char comment[31];
+    char genre;
+
+    Id3TagInfo() {
+	    contains_info = false;
+	    nullinit();
+    }
+
+    Id3TagInfo(char *buffer) {
+	    if (buffer && (!strncmp(buffer,"TAG",3))) {
+	        memset(this,0,sizeof(Id3TagInfo));
+	        contains_info = true;
+	        strncpy(songname, buffer + 3, 30);
+	        strncpy(artist, buffer + 33, 30);
+	        strncpy(album, buffer + 63, 30);
+	        strncpy(year, buffer + 93, 4);
+	        strncpy(comment, buffer + 97, 30);
+	        genre = buffer[127];
+	    } else {
+	        contains_info = false;
+	        nullinit();
+	    }
+    }
+
+ private:
+    void nullinit() {
+	    *songname = '\0';
+	    *artist = '\0';
+	    *album = '\0';
+	    *year = '\0';
+	    *comment = '\0';
+	    genre = '\0';
+    }
+};
 
 
-#endif /* _EVENTDATA_H_ */
+#endif /* _ID3V1_H_ */
 
 
 
