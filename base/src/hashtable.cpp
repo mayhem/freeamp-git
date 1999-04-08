27aa50df31e04187f3ff30d5cece2e2e35c058c5
elrod <>
1999-04-08 07:33:46 +0000

    * added "Valters Vingolds" <Valters_Vingolds@swh-t.lv> to authors * fixed messed up cvs header in hashtable.cpp

--- base/src/hashtable.cpp
+++ base/src/hashtable.cpp
@@ -18,9 +18,8 @@
 	along with this program; if not, write to the Free Software
 	Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
 	
-	$$
+	$Id$
 ____________________________________________________________________________*/
-
 
 #include <stdlib.h>
 #include <memory.h>
