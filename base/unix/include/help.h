4a498ced9f2440dd06e9e3fc2ffc486303b984b3
robert <>
1999-11-10 02:27:59 +0000

    Added support for unix help. The program expects the help files to be in {installdir}/freeamp/help/freeamp.html. The freeamphelp.tar.gz file will create the help directory, so it needs to get unzipped in {installdir>/freeamp during the install.

--- base/unix/include/help.h
+++ base/unix/include/help.h
@@ -24,28 +24,6 @@ ____________________________________________________________________________*/
 #ifndef INCLUDED_HELP_H
 #define INCLUDED_HELP_H
 
-#define HELP_FILE "freeamp.hlp"
-
-//Main Dialog by default in Help_Finder
-#define FreeAmp_Main_Window	0x0	
-
-//Music Browser
-#define Music_Browser 1
-//Actual name of dialog for drive selection unknown
-//Temporary name used to allocate ID number.
-#define Music_Browser_Drive_Selection 10
-
-//Download Manager
-#define Download_Manager 2
-
-//Preferences Dialog
-#define Preferences_General 3
-#define Preferences_Themes 4
-#define Preferences_Streaming 5
-#define Preferences_Update 6
-#define Preferences_Plugins 7
-#define Preferences_Advanced 8
-#define Preferences_About 9
-
+#define HELP_FILE "freeamp/help/freeamp.html"
 
 #endif
