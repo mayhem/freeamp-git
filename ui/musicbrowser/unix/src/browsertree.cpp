d94dc25e4ee361245663e5ecbf75afd5ea167ea7
ijr <>
2000-09-15 09:30:40 +0000

    - remove debugging cout

--- ui/musicbrowser/unix/src/browsertree.cpp
+++ ui/musicbrowser/unix/src/browsertree.cpp
@@ -146,7 +146,6 @@ vector<PlaylistItem *> *GTKMusicBrowser::GetTreeSelection(void)
             for (; j != list->m_trackList->end(); j++) {
                 PlaylistItem *item = new PlaylistItem(*(PlaylistItem *)*j);
                 newlist->push_back(item);
-cout << "adding " << item->URL() << endl;
             }
             break; }
         case kTreeCD:
